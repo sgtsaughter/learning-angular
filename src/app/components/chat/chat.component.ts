02e1ceb7e1a3d57819c738a6da05f96f93f202cc
Patrick <patrick@Installs-MacBook-Pro.local>
2018-10-22 13:36:21 -0400

    Added lodash library and created chatBot() in chat component.

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../../services/socket.service';
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,7 @@ export class ChatComponent implements OnInit {
   messages: Array<any>;
   avatar: string = 'https://api.adorable.io/avatars/30/abott@adorable.png';
   selfAuthored: boolean = false;
+  botMessage: string;
 
 
   constructor(private _socketService: SocketService) { }
@@ -29,11 +31,41 @@ export class ChatComponent implements OnInit {
     const message = {
       text: this.messageText,
       date: Date.now(),
-      imageUrl: this.avatar
+      imageUrl: this.avatar,
+      isBot: false
     };
+    console.log(message);
     this._socketService.emit('send-message', message);
     this.messageText = '';
+    this.chatBot();
   }
 
+  chatBot() {
+    const responseArray = [
+      'GGWWWRGHH',
+      'GGWWWRGHH',
+      'AAARARRRGWWWH',
+      'WWWWWWWGGGGHHHRRRRW',
+      'WAGRRRRWWGAHHHHWWWRRGGAWWWWWWRR',
+      'Aaaaaarrrrr rhhhnnn gggggrrrrr',
+      'Rhhhnngggnn. Garrrrr!',
+    ];
+
+    const botMessage = _.sample(responseArray);
+    console.log(botMessage)
+
+    const message = {
+      text: botMessage,
+      date: Date.now(),
+      imageUrl: '/src/app/images/chewbacca_icon.png',
+      isBot: true
+    };
+    
+    console.log(message);
+    //Set a timeout to give the illusion that the bot is taking time to think of a response. 
+    setTimeout(() => {
+      this._socketService.emit('send-message', message);
+    }, 2000)
+  }
 
 }
